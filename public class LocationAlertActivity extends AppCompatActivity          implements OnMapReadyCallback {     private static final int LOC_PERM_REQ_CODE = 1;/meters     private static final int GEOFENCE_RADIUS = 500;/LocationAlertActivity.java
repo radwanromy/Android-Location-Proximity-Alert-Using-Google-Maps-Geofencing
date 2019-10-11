f6f05601293d89c8e433f94753e5e9ee090b1a05
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2019-10-11 10:19:32 -0700

    Create LocationAlertActivity.java

---
+++ public class LocationAlertActivity extends AppCompatActivity          implements OnMapReadyCallback {     private static final int LOC_PERM_REQ_CODE = 1;/meters     private static final int GEOFENCE_RADIUS = 500;/LocationAlertActivity.java
@@ -0,0 +1,174 @@
+public class LocationAlertActivity extends AppCompatActivity 
+        implements OnMapReadyCallback {
+
+    private static final int LOC_PERM_REQ_CODE = 1;
+    //meters
+    private static final int GEOFENCE_RADIUS = 500;
+    //in milli seconds
+    private static final int GEOFENCE_EXPIRATION = 6000;
+
+    private GoogleMap mMap;
+
+    private GeofencingClient geofencingClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location_alert_layout);
+
+        Toolbar tb = findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+        tb.setSubtitle("Location Alert");
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.g_map);
+        mapFragment.getMapAsync(this);
+
+        geofencingClient = LocationServices.getGeofencingClient(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setMinZoomPreference(15);
+
+        showCurrentLocationOnMap();
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                addLocationAlert(latLng.latitude, latLng.longitude);
+            }
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    private void showCurrentLocationOnMap() {
+        if (isLocationAccessPermitted()) {
+            requestLocationAccessPermission();
+        } else if (mMap != null) {
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+    private boolean isLocationAccessPermitted(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    private void requestLocationAccessPermission(){
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                LOC_PERM_REQ_CODE);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void addLocationAlert(double lat, double lng){
+        if (isLocationAccessPermitted()) {
+            requestLocationAccessPermission();
+        } else  {
+            String key = ""+lat+"-"+lng;
+            Geofence geofence = getGeofence(lat, lng, key);
+            geofencingClient.addGeofences(getGeofencingRequest(geofence),
+                    getGeofencePendingIntent())
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LocationAlertActivity.this,
+                                        "Location alter has been added",
+                                        Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(LocationAlertActivity.this,
+                                        "Location alter could not be added",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+    private void removeLocationAlert(){
+        if (isLocationAccessPermitted()) {
+            requestLocationAccessPermission();
+        } else {
+            geofencingClient.removeGeofences(getGeofencePendingIntent())
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LocationAlertActivity.this,
+                                        "Location alters have been removed",
+                                        Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(LocationAlertActivity.this,
+                                        "Location alters could not be removed",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case LOC_PERM_REQ_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showCurrentLocationOnMap();
+                    Toast.makeText(LocationAlertActivity.this,
+                            "Location access permission granted, you try " +
+                                    "add or remove location allerts",
+                            Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+        }
+    }
+    private PendingIntent getGeofencePendingIntent() {
+        Intent intent = new Intent(this, LocationAlertIntentService.class);
+        return PendingIntent.getService(this, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+    private GeofencingRequest getGeofencingRequest(Geofence geofence) {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_DWELL);
+        builder.addGeofence(geofence);
+        return builder.build();
+    }
+
+    private Geofence getGeofence(double lat, double lang, String key) {
+        return new Geofence.Builder()
+                .setRequestId(key)
+                .setCircularRegion(lat, lang, GEOFENCE_RADIUS)
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                        Geofence.GEOFENCE_TRANSITION_DWELL)
+                .setLoiteringDelay(10000)
+                .build();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.remove_loc_alert:
+                removeLocationAlert();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+}
